0236820269a7e259c13769ccd54f74ba5d98693f
Jiang Ji <jiang.ji@check24.de>
2019-05-04 00:31:44 +0200

    creating classes

--- agent/agents.py
+++ agent/agents.py
@@ -15,12 +15,13 @@ class Monkey:
                  productivity = 1.0,
                  luck = 1.0,
                  resource: float = 1.0,
-                 well_being: float = 1.0):
+                 id=-1):
         self.morality_gene = morality_gene
         self.resource = resource
         self.well_being = np.sqrt(self.resource)
         self.productivity = productivity,
         self.luck = luck
+        self.id = id
 
     def produce(self, ):
         pass
@@ -46,14 +47,18 @@ class MonkeySpecies:
                  size: int,
                  id: int = -1,
                  interaction_rate = 0.1):
-        self.monkeys = [Monkey(morality_gene=morality_gene)] * size
-        self.id = id
+        self.monkeys = []
+        for i in range(size):
+            self.monkeys.append(Monkey(morality_gene,
+                                       id = i))
         self.interaction_rate = interaction_rate
         self.n_interactions = np.round(self.interaction_rate * size)
+        self.id = id
 
     def interact(self):
         for monkey in randomly(self.monkeys):
-            other_monkeys = random.choices(monkey, k=self.n_interactions)
+            all_other_monkeys =
+            other_monkeys = random.choices(self.monkeys, k=self.n_interactions)
             for other_monkey in other_monkeys:
                 monkey.interact(other_monkey)
 
